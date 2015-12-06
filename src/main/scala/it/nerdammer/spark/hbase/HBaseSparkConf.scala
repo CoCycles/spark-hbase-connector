a7c14b2a5924baf84a779c747674fe7822ac481e
Ran Mizrahi <ran@cocycles.com>
2015-12-06 15:10:18 +0200

    feat(config): supported hbase-site.xml
    now hbase-site config can be added

--- src/main/scala/it/nerdammer/spark/hbase/HBaseSparkConf.scala
+++ src/main/scala/it/nerdammer/spark/hbase/HBaseSparkConf.scala
@@ -5,14 +5,17 @@ import org.apache.spark.SparkConf
 
 case class HBaseSparkConf (
   hbaseHost: String = HBaseSparkConf.DefaultHBaseHost,
+  hbaseSitePath: String = null,
   hbaseRootDir: String = HBaseSparkConf.DefaultHBaseRootDir) extends Serializable {
 
   def createHadoopBaseConfig() = {
     val conf = HBaseConfiguration.create
+
     conf.setBoolean("hbase.cluster.distributed", true)
-    conf.setInt("hbase.client.scanner.caching", 10000)
     conf.set("hbase.rootdir", hbaseRootDir)
     conf.set("hbase.zookeeper.quorum", hbaseHost)
+
+    if (hbaseSitePath != null) conf.addResource(hbaseSitePath)
 
     conf
   }
@@ -25,6 +28,7 @@ object HBaseSparkConf extends Serializable {
   def fromSparkConf(conf: SparkConf) = {
     HBaseSparkConf(
       hbaseHost = conf.get("spark.hbase.host", DefaultHBaseHost),
+      hbaseSitePath = conf.get("spark.hbase.site", null),
       hbaseRootDir = conf.get("spark.hbase.root.dir", DefaultHBaseRootDir)
     )
   }
